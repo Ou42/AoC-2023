f4b20c69fd07e2eb7aa43b4fe9893d2ddbfb38ef
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-12-01 02:06:39 -0800

    day-01-part-b tests pass
    - clunky
    - needs clearer parsing

--- days/01/day-01.hs
+++ days/01/day-01.hs
@@ -1,7 +1,8 @@
 module Main where
 
 import Data.Char (isDigit)
-import Data.List (isPrefixOf)
+import Data.List (elemIndex, isPrefixOf)
+import Data.Maybe (fromJust)
 
 {-
     Day 01
@@ -26,33 +27,76 @@ hr = putStrLn $ replicate 42 '-' ++ ['\n']
 
 partA :: String -> Int
 partA fileStr = sum $ map go $ lines fileStr
-  where 
+  where
     go :: String -> Int
     go lineStr =
       let leftDigit = head . snd $ break isDigit lineStr
           rghtDigit = head . snd $ break isDigit $ reverse lineStr
       in read $ leftDigit : [rghtDigit]
 
--- partB :: String -> Int
--- partB fileStr = sum $ map go $ lines fileStr
-partB fileStr = sum $ map go $ lines fileStr
+
+numWords = [ "one", "two", "three"
+           , "four", "five", "six"
+           , "seven", "eight", "nine"
+           ]
+
+go2 :: String -> Maybe Int
+go2 []    = Nothing
+go2 chars =
+  let lookUp = map (\numWord -> numWord `isPrefixOf` chars) numWords
+  in  if all not lookUp
+        then go2 $ tail chars
+        else (+1) <$> elemIndex True lookUp
+
+findLast :: Maybe String -> Maybe Int
+findLast Nothing    = Nothing
+findLast (Just str) = go3 Nothing str
   where
-    numWords = [ "one", "two", "three"
-               , "four", "five", "six"
-               , "seven", "eight", "nine"
-               ]
-    go :: String -> Int
-    go lineStr = undefined
+    go3 accu []    = accu
+    go3 accu chars@(c:_) =
+      let lookUp = map (\numWord -> numWord `isPrefixOf` chars) numWords
+      in  if isDigit c
+            then go3 (Just (read [c])) $ tail chars
+            else if all not lookUp
+                    then go3 accu $ tail chars
+                    else go3 ((+1) <$> elemIndex True lookUp) $ tail chars
+
+
+-- using: isPrefixOf & elemIndex
+-- partB :: String -> Int
+partB fileStr = map go $ lines fileStr
+-- partB fileStr = sum $ map go $ lines fileStr
+  where
+    go :: [Char] -> Int
+    go lineStr =
+      let (upUntilDigit, afterFirstDigit) = break isDigit lineStr
+          (firstDigit, rest) = if null afterFirstDigit
+                                  then (Nothing, Just upUntilDigit)
+                                  else ( Just $ read [head afterFirstDigit]
+                                      , Just $ tail afterFirstDigit)
+          (d1, d2, d3) = (go2 upUntilDigit, firstDigit, findLast rest)
+          tens = if null d1
+                    then fromJust d2
+                    else fromJust d1
+          ones = if null d3
+                    then tens
+                    else fromJust d3
+      in 10 * tens + ones
 
 main :: IO ()
 main = do
-  -- fileInput <- readFile "input-01-part-a.test"
-  -- fileInput <- readFile "input-01-part-b.test"
-  fileInput <- readFile "input-01.txt"
+  fileInputA <- readFile "input-01-part-a.test"
+  fileInputB <- readFile "input-01-part-b.test"
+  -- fileInputA <- readFile "input-01.txt"
+  -- fileInputB <- readFile "input-01.txt"
 
   putStrLn "Day 01 - Part A"
-  putStrLn fileInput
+  putStrLn $ unlines $ take 3 $ lines fileInputA
 
   hr
 
-  print $ "Part A = " ++ show (partA fileInput)
+  print $ "Part A = " ++ show (partA fileInputA)
+
+  hr
+
+  print $ "Part B = " ++ show (partB fileInputB)
