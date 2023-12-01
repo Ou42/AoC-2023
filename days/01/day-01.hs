17f3e5f507652740286dcd39bf8821cbc518a468
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-11-30 23:55:39 -0800

    day-01-part-b WIP
    - 2nd test input for Part B
    - renamed Part A's test input
    - parse number words?!

--- days/01/day-01.hs
+++ days/01/day-01.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char (isDigit)
+import Data.List (isPrefixOf)
 
 {-
     Day 01
@@ -10,22 +11,43 @@ import Data.Char (isDigit)
         from each line
       . Put them together to form a 2 digit number
       . Sum each line's numbers
+
+      Part B
+      . ... some of the digits are actually spelled out with letters:
+            one, two, three, four, five, six, seven, eight, and nine
+        also count as valid "digits".
+      . the rest is the same, find the first and last "digits"
+      . combine them into a 2 digit number
+      . sum each line's numbers
 -}
 
 hr :: IO ()
 hr = putStrLn $ replicate 42 '-' ++ ['\n']
 
--- partA :: String -> String
+partA :: String -> Int
 partA fileStr = sum $ map go $ lines fileStr
-go :: String -> Int
-go lineStr =
-  let leftDigit = head . snd $ break isDigit lineStr
-      rghtDigit = head . snd $ break isDigit $ reverse lineStr
-  in read $ leftDigit : [rghtDigit]
+  where 
+    go :: String -> Int
+    go lineStr =
+      let leftDigit = head . snd $ break isDigit lineStr
+          rghtDigit = head . snd $ break isDigit $ reverse lineStr
+      in read $ leftDigit : [rghtDigit]
+
+-- partB :: String -> Int
+-- partB fileStr = sum $ map go $ lines fileStr
+partB fileStr = sum $ map go $ lines fileStr
+  where
+    numWords = [ "one", "two", "three"
+               , "four", "five", "six"
+               , "seven", "eight", "nine"
+               ]
+    go :: String -> Int
+    go lineStr = undefined
 
 main :: IO ()
 main = do
-  -- fileInput <- readFile "input-01.test"
+  -- fileInput <- readFile "input-01-part-a.test"
+  -- fileInput <- readFile "input-01-part-b.test"
   fileInput <- readFile "input-01.txt"
 
   putStrLn "Day 01 - Part A"
