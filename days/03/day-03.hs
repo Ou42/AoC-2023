328ce9e6aaef9d74239c6a973299a81a69b49522
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-12-03 23:53:21 -0800

    day-03-part-a plan of attack

--- days/03/day-03.hs
+++ days/03/day-03.hs
@@ -20,6 +20,23 @@ partA filePath = do
 
   hr
 
+  -- I'm thinking of:
+  --   1. reading/parsing the input data
+  --   2. scaning for a number
+  --   3. then searching all locations around the number
+  --   4. if a non `.` symbol is found, it's a "part number"
+  --   5. goto 2, until the whole 2D grid is processed
+  --   6. sum the list of "part numbers"
+
+  -- As opposed to:
+  --   - scanning for symbols and then trying to extract adjacent numbers
+
+  -- Potentially useful tools:
+  --   1. Data.Char.isPunctuation ?!
+  --   2. ghci> Data.Maybe.catMaybes [Just 6, Just 7, Nothing]
+  --      [6,7]
+  --   3. zip line [0..] to get indices
+  --   4. use Array's or a data structure that can extract a sub-sequence by indices
 
 main :: IO ()
 main = do
