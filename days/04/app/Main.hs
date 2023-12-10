4791c470b436ce3fdc6778feba2349d39a3cb157
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-12-09 22:19:52 -0800

    day-04-part-a parsing done
    - using break, words, read

--- days/04/app/Main.hs
+++ days/04/app/Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import qualified Data.List as L
+import qualified Data.Set as S
+import Data.Set (Set)
+
 {-
     Day 04
 
@@ -12,6 +16,27 @@ module Main where
       - Score 1 point for the first match, then double the points for ea match after the 1st
       - What is the total number of points?
 -}
+type Card = ([Char], (Set Int, Set Int))
+
+parseDay04 :: String -> [Card]
+parseDay04 fileInput =  L.map ( fmap ( go
+                                     . fmap (L.drop 1)
+                                     . break (== '|')
+                                     . L.drop 1 )
+                              . break (== ':') 
+                              . L.drop 4)
+                        $ lines fileInput
+  where
+    go :: (String, String) -> (Set Int, Set Int)
+    go (win, picked) = ( S.fromList $ L.map read $ words win
+                       , S.fromList $ L.map read $ words picked )
+
+
+partA :: [Card] -> Int
+partA = undefined
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  fileInput <- readFile "input-04.test"
+--   fileInput <- readFile "input-04.txt"
+  putStrLn $ unlines $ L.map show $ parseDay04 fileInput
