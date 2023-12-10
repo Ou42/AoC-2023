dff21664271274659b910dc779ea417ca494b9e7
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-12-09 20:50:16 -0800

    day-04-part-a Started

---
+++ days/04/app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+{-
+    Day 04
+
+      Part A
+      - given a list of numbered Cards
+      - each card conatins 2 lists:
+        - a list of winning numbers
+        - a list of numbers we have
+      - figure out which of the numbers you have appear in the list of winning numbers
+      - Score 1 point for the first match, then double the points for ea match after the 1st
+      - What is the total number of points?
+-}
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
